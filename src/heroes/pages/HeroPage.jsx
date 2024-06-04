1001214d365d70d971dfbc7dee69cb83aadd35ca
camilobriceno <camilobriceno@hotmail.es>
2024-06-04 21:17:58 +0200

    Initial commit

---
+++ src/heroes/pages/HeroPage.jsx
@@ -0,0 +1,52 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers";
+import { useMemo } from "react";
+
+
+export const HeroPage = () => {
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const hero = useMemo( () => getHeroById( id ), [ id ]  );
+
+  if ( !hero ) {
+    return <Navigate to="/marvel" />
+  }
+
+  const onNavigateBar = () => {
+
+    return navigate("/search");
+  }
+
+  return (
+    <div className="row mt-5 m-5">
+      <div className="col-4">
+        <img
+        src={ `/heroes/${ id }.jpg` }
+        alt= { hero.superhero }
+        className="img-thumbnail animate__animated animate__fadeInLeft"
+        />
+      </div>
+
+      <div className="col-8 mt-2">
+        <h3>{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush" >
+          <li className="list-group-item"> <b>Alter Ego:</b> { hero.alter_ego } </li>
+          <li className="list-group-item"> <b>Publisher:</b> { hero.publisher } </li>
+          <li className="list-group-item"> <b>First Appereance:</b> { hero.first_appearance } </li>
+
+          <h5 className="mt-3"> Characters </h5>
+          <p>{ hero.characters }</p>
+
+          <button
+          className="btn btn-outline-primary"
+          onClick={ onNavigateBar }
+          >
+            Regresar
+          </button>
+        </ul>
+      </div>
+    </div>
+  )
+}
---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,16 @@
+import { HeroList } from "../components/HeroList"
+
+export const MarvelPage = () => {
+  return (
+
+    <>
+
+    <h1 className="text-center">
+      MarvelPage
+    </h1>
+    <hr />
+
+    <HeroList publisher='Marvel Comics'/>
+    </>
+  )
+}
